fb5cb69e62b2fd8c881f61f80523ca4a10a1a2b3
MattiaBologna <mattiabologna01@gmail.com>
2024-05-09 16:40:57 +0200

    Add: Production class

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+
+class Production {
+    public $title;
+    public $language;
+    public $vote;
+
+    public function setTitle(string $_title): void  {
+        $this->title = $_title;
+    }
+
+    public function setLanguage(string $_language): void {
+        $this->language = $_language;
+    }
+
+    public function setVote(int $_vote): void {
+        $this->vote = $_vote;
+    }
+
+    function __construct(string $_title, string $_language, int $_vote) {
+        $this->setTitle($_title);
+        $this->setLanguage($_language);
+        $this->setVote($_vote);
+    } 
+}
+
+$avatar = new Production('Avatar', 'english', 8);
+$inception = new Production('Inceprion', 'italian', 7);
+
+var_dump($avatar);
+var_dump($inception);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
