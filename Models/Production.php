147bf4442b795b7c100149f64080b61f2680873c
MattiaBologna <mattiabologna01@gmail.com>
2024-05-10 16:20:33 +0200

    Add: cards layout

--- Models/Production.php
+++ Models/Production.php
@@ -1,27 +1,61 @@
 <?php
 
+// class Production {
+//     public $title;
+//     public $language;
+//     public $vote;
+
+//     public function setTitle(string $_title): void  {
+//         $this->title = $_title;
+//     }
+
+//     public function setLanguage(string $_language): void {
+//         $this->language = $_language;
+//     }
+
+//     public function setVote(int $_vote): void {
+//         $this->vote = $_vote;
+//     }
+
+//     function __construct(string $_title, string $_language, int $_vote) {
+//         $this->setTitle($_title);
+//         $this->setLanguage($_language);
+//         $this->setVote($_vote);
+//     } 
+
+//     public function getTitle() {
+//         return $this->$title;
+//     }
+//     public function getLanguage() {
+//         return $this->$language;
+//     }
+//     public function getVote() {
+//         return $this->$_vote;
+//     }
+// }
+
 class Production {
     public $title;
     public $language;
-    public $vote;
+    public $rating;
 
-    public function setTitle(string $_title): void  {
-        $this->title = $_title;
+    public function __construct($title, $language, $rating) {
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
     }
 
-    public function setLanguage(string $_language): void {
-        $this->language = $_language;
+    public function getTitle() {
+        return $this->title;
     }
 
-    public function setVote(int $_vote): void {
-        $this->vote = $_vote;
+    public function getLanguage() {
+        return $this->language;
     }
 
-    function __construct(string $_title, string $_language, int $_vote) {
-        $this->setTitle($_title);
-        $this->setLanguage($_language);
-        $this->setVote($_vote);
-    } 
+    public function getRating() {
+        return $this->rating;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 require_once __DIR__ . '/Models/Production.php';
 
-$avatar = new Production('Avatar', 'english', 8);
-$inception = new Production('Inceprion', 'italian', 7);
+$productions = [
+    new Production('Avatar', 'english', 8),
+    new Production('Inceprion', 'italian', 7),
+    new Production('Pippo', 'gibberish', 10),
+];
 
-var_dump($avatar);
-var_dump($inception);
+// var_dump($productions);
 
 ?>
 <!DOCTYPE html>
@@ -15,10 +17,39 @@ var_dump($inception);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
-    ciao
+    <div id="app">
+        
+    <main>
+        <div class="container">
+            <div class="row">
+                <?php 
+                    for ($i = 0; $i < count($productions); $i++) {
+                        ?>
+                            <div class="card col-4 m-4" style="width: 18rem;">
+                                <img src="https://www.desktopbackground.org/download/1280x900/2011/06/03/213403_movies-generic-pickupmovie-home-theater-backdrops-wallpapers_1920x1080_h.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $productions[$i]->getTitle(); ?></h5>
+                                    <p class="card-text"><?php echo $productions[$i]->getLanguage(); ?></p>
+                                    <p class="card-text"><?php echo $productions[$i]->getRating(); ?></p>
+                                </div>
+                            </div>
+                        <?php 
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
 
+
+
+    </div>
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
